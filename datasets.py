95271e8cb96eb0e93c7c8e93fe986e9fdc38f809
endaaman <ken@endaaman.me>
2023-05-13 23:17:12 +0900

    update

--- datasets.py
+++ datasets.py
@@ -1,3 +1,4 @@
+import os
 from glob import glob
 from typing import NamedTuple
 
@@ -14,23 +15,37 @@ class Item(NamedTuple):
     name: str
     label: str
     image: ImageType
+    index: int
 
 
 class ImageDataset(Dataset):
-    def __init__(self):
-        items = self.load_data()
+    def __init__(self, image_size=512):
+        self.image_size = image_size
+        self.items = self.load_data()
 
     def load_data(self):
         labels = ['FM', 'SFT']
+        items = {}
         for label in labels:
-            print(label)
             basedir = f'datasets/FM_vs_SFT/{label}'
             for path in sorted(glob(f'{basedir}/*.jpg')):
-                i = Image.open(path)
-
-        return []
+                image = Image.open(path)
+                name = os.path.splitext(os.path.basename(path))[0]
+                ii = grid_split(image, self.image_size, overwrap=False, flattern=True)
+                for idx, i in enumerate(ii):
+                    item = Item(
+                        path=path,
+                        name=name,
+                        label=label,
+                        image=i,
+                        index=idx,
+                    )
+                    if name in items:
+                        items[name].append(item)
+                    else:
+                        items[name] = [item]
+        return items
 
 
 if __name__ == '__main__':
     ds = ImageDataset()
-
